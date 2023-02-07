d8f599a047d7f3bdf70305096e95568d46a0f709
samandar92 <mavlonovsamandar1992@gmail.com>
2023-02-07 23:09:56 +0500

    fix

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter as Router, Route,  Routes } from "react-router-dom";
+import EditPage from "./components/EditPage";
+import GetData from "./components/GetData/GetData";
+import Header from "./components/Header/Header";
+import NavbarFunc from "./components/Navbar";
+
+
+
+
+function App() {
+
+
+
+  return (
+   <>
+  <Router>
+   <NavbarFunc/>
+    <Routes>
+      <Route path="/home" element={<Header />} />
+      <Route path="/result" element={<GetData />} />
+      <Route path="/result/edit/:id" element={<EditPage />} />
+    </Routes>
+  </Router>
+
+  
+   </>
+  );
+}
+
+export default App;
